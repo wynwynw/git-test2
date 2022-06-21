4e89fb4328c2dd6daf9dd3e75154055eb3d61038
wangyanan <2716358553@qq.com>
2022-06-21 16:57:50 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hi git");
         System.out.println("hi git2");
+        System.out.println("hi git3");
     }
 }
