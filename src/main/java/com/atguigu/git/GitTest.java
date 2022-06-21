f0cbae7ccf0466986a25f926b2a4e7224e94d799
wangyanan <2716358553@qq.com>
2022-06-21 16:54:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Auther:wangyanan
+ * @Date:2022/6/21 16:53
+ * @Compriton:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hi git");
+    }
+}
